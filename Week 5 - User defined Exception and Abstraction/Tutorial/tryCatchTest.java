51f5548248b2c744302477c20bb7f3222338765c
dheerazsah <83595627+dheerazsah@users.noreply.github.com>
2023-01-21 14:19:28 +0545

    tryCatchTest.java pushed

---
+++ Week 5 - User defined Exception and Abstraction/Tutorial/tryCatchTest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;  
+
+public class tryCatchTest{
+    public static void main(String[] args){
+        int num;
+   
+      try {
+        Scanner input = new Scanner(System.in);
+        // ask users to enter numbers
+        System.out.println("Enter a number: ");
+        num = input.nextInt();
+      }
+      catch (InputMismatchException e) { // Handles error for datatypes 
+        System.out.println("Invalid.");
+      }
+}
+}
