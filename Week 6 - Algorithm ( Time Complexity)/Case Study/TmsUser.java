b06db2e792f60c00480fb62faa06d654498cb1fb
dheerazsah <83595627+dheerazsah@users.noreply.github.com>
2023-01-23 07:08:27 +0545

    tmsuser.java pushed

---
+++ Week 6 - Algorithm ( Time Complexity)/Case Study/TmsUser.java
@@ -0,0 +1,7 @@
+public abstract class TmsUser { // Using concept of abstraction 
+
+    public String username; // Declaring a variable for username
+    public String password; //Declaring a variable for password
+
+    public abstract void login(); //Object call
+}
